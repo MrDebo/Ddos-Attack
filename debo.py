19a02ff22cdaf2d4d876f8288238f715cd34a00f
MrDebo <57215976+MrDebo@users.noreply.github.com>
2019-11-14 21:50:34 +0700

    Update debo.py

--- debo.py
+++ debo.py
@@ -30,21 +30,21 @@ port = input("Port       : ")
 
 os.system("clear")
 os.system("figlet Attack Starting")
-print "[                    ] 0% "
+print "[====                    ] 0%"
 time.sleep(5)
-print "[====                 ] 15%"
+print "[=======                 ] 15%"
 time.sleep(5)
-print "[======               ] 25%"
+print "[==========              ] 25%"
 time.sleep(5)
-print "[=========            ] 35%"
+print "[=============           ] 35%"
 time.sleep(5)
-print "[===========          ] 50%"
+print "[================        ] 50%"
 time.sleep(5)
-print "[==============       ] 75%"
+print "[===================     ] 75%"
 time.sleep(5)
-print "[=================    ] 85%"
+print "[======================  ] 85%"
 time.sleep(5)
-print "[====================] 100%"
+print "[========================] 100%"
 time.sleep(3)
 sent = 0
 while True:
